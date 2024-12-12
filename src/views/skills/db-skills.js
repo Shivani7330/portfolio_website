37a60dcabf64081f60daf8d6bcda733286db7d4b
Shivani7330 <shivanihallade@gmail.com>
2024-12-12 21:53:22 +0530

    Fix ESLint unused variable issues in Skills.js and db-skills.js

--- src/views/skills/db-skills.js
+++ src/views/skills/db-skills.js
@@ -1,18 +1,18 @@
 import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
-import L_SASS from "../../assets/img/skills/sass-1.svg";
+
 import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
 import L_REACT from "../../assets/img/skills/react.svg";
 import L_REDUX from "../../assets/img/skills/redux.svg";
-import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+
 import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
 import L_GIT from "../../assets/img/skills/git-icon.svg";
-// import L_HEROKU from "../../assets/img/skills/heroku.svg";
+
 import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
 import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
 import L_NETLIFY from "../../assets/img/skills/netlify-icon.svg"
 import L_FIREBASE from "../../assets/img/skills/firebase-icon.svg"
-import L_VERCEL from "../../assets/img/skills/logo-vercel.svg"
+
 import L_TAILWIND from "../../assets/img/skills/tailwindcss-icon.svg"
 
 
