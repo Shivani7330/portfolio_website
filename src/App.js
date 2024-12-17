bee01f6b09ea11fd07db24a0fdc007729c5394c5
Shivani Hallade <167903122+Shivani7330@users.noreply.github.com>
2024-12-17 21:09:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
-// import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Skills from "./views/skills/Skills";
